cdd21da1ecd69b14690439f78e0f46dbc6a94a9a
tangyunhang <18815292164@163.com>
2023-09-13 16:43:28 +0800

    mac-alibaba

--- msc-alibaba-showinfo/src/main/java/com/abc/controller/TestController.java
+++ msc-alibaba-showinfo/src/main/java/com/abc/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.abc.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,10 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("testInfo")
 public class TestController {
+    @Autowired
+    private StringRedisTemplate template;
+
+
     @RequestMapping("header")
     public String header(HttpServletRequest httpServletRequest){
         return "header x-reqyest-red:"+ httpServletRequest.getHeader ("x-reqyest-red");
